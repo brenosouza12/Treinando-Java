f058e6636f3a01cf10a6dbf648d1c705230c24d7
brenosouza12 <brenosouzadev12l@gmail.com>
2024-08-21 17:21:17 -0300

    20240821

--- 20240821_Calculadora/src/App.java
+++ 20240821_Calculadora/src/App.java
@@ -7,15 +7,15 @@ public class App {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Digite o primeiro número: ");
-        double num1 = scanner.nextDouble();
+        double numero1 = scanner.nextDouble();
 
         System.out.println("Digite o segundo número: ");
-        double num2 = scanner.nextDouble();
+        double numero2 = scanner.nextDouble();
 
-        double soma = num1 + num2;
-        double subtracao = num1 - num2;
-        double multiplicacao = num1 * num2;
-        double divisao = num1 / num2;
+        double soma = numero1 + numero2;
+        double subtracao = numero1 - numero2;
+        double multiplicacao = numero1 * numero2;
+        double divisao = numero1 / numero2;
 
         System.out.println("Soma: " + soma);
         System.out.println("Subtração: " + subtracao);
